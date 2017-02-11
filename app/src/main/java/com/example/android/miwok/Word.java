da166ee5ce0836b462cc28ab3a210d6c4fd8f702
Bohdan Tanchak <fpmtanchak@gmail.com>
2017-02-11 23:36:45 +0200

    Add different colors to activities & make different constractors in WordAdapter

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -2,24 +2,41 @@ package com.example.android.miwok;
 
 /**
  * Created by Bohdan Tanchak on 06.02.2017.
- *
  */
 
 public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourseID = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
 
-    public Word(String miwokTranslation, String defaultTranslation){
+    public Word(String miwokTranslation, String defaultTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
 
-    public String getmDefaultTranslation(){
+    public Word(String miwokTranslation, String defaultTranslation, int imageResourseID) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourseID = imageResourseID;
+    }
+
+    public String getmDefaultTranslation() {
         return mDefaultTranslation;
     }
 
-    public String getmMiwokTranslation(){
+    public String getmMiwokTranslation() {
         return mMiwokTranslation;
     }
+
+    public int getImageResourceId() {
+        return mImageResourseID;
+    }
+
+    public boolean hasImage() {
+        return mImageResourseID != NO_IMAGE_PROVIDED;
+    }
+
+
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,18 +19,20 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
+    private int mColorResourceId;
 
-    public WordAdapter(Activity context, ArrayList<Word> words) {
-        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
-        // the second argument is used when the ArrayAdapter is populating a single TextView.
-        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
-        // going to use this second argument, so it can be any value. Here, we used 0.
+    public WordAdapter(Activity context, ArrayList<Word> words ) {
+
         super(context, 0, words);
+    }
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId ) {
+
+        super(context, 0, words);
+        mColorResourceId = colorResourceId;
     }
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
-
 
 
         // Check if the existing view is being reused, otherwise inflate the view
@@ -44,15 +47,37 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         // Find the TextView in the list_item.xml layout with the ID version_name
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+
+
         // Get the version name from the current AndroidFlavor object and
         // set this text on the name TextView
         miwokTextView.setText(currentWord.getmMiwokTranslation());
 
         // Find the TextView in the list_item.xml layout with the ID version_number
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+
+
         // Get the version number from the current AndroidFlavor object and
         // set this text on the number TextView
         defaultTextView.setText(currentWord.getmDefaultTranslation());
+
+        // Find the ImageView in the list_item.xml layout with the ID image.
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+
+        if( currentWord.hasImage()){
+            // Set the ImageView to the image resource specified in the current Word
+            imageView.setImageResource(currentWord.getImageResourceId());
+
+            imageView.setVisibility(View.VISIBLE);
+        }else{
+            imageView.setVisibility(View.GONE);
+        }
+
+        View textConteiner = listItemView.findViewById(R.id.text_conteiner);
+
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        textConteiner.setBackgroundColor(color);
+
 
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
